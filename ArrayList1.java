4856ab385b1f04a8660d32ba46a1367dfd153d4b
pritifating <pritifating7@gmail.com>
2021-02-09 16:59:18 +0530

    collectionFile

---
+++ ArrayList1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class ArrayList1
+{
+	public static void main(String args[])
+	{
+		ArrayList list=new ArrayList();
+		list.add("priti");
+		//list.add(2);
+		//list.add(3);
+		// System.out.println(list);
+	}
+}
